37afc620af832dc16dd6d2bb815613bcf90e3c89
Luuk Brauckmann <luukbrauckmann@gmail.com>
2022-09-07 12:05:25 +0200

    Dark theme Poker table Cards Card flip animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
-// const expandButton = document.querySelector('header button')
-// expandButton.addEventListener('click', expand)
+function toggleDeck () {
+	const cards = document.getElementById('community-cards').children
+	for (let card of cards) card.classList.toggle('in-deck')
+}
 
-// function expand () {
-//   document.body.classList.toggle('expand')
-// }
+function toggleCard (id) {
+	const card = document.getElementById(`card-${id}`)
+	card.classList.toggle('in-deck')
+}
