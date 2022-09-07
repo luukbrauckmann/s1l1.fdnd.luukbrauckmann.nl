5d346747394fd2d554b61e71dedf63699214a541
Luuk Brauckmann <luukbrauckmann@gmail.com>
2022-09-07 15:15:29 +0200

    Nieuwe layout profile picture Hide en add functies Styling aangepast

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,24 @@
-function toggleDeck () {
+function hideCards () {
 	const cards = document.getElementById('community-cards').children
-	for (let card of cards) card.classList.toggle('in-deck')
+	for (let card of cards) {
+		const hasClass = card.classList[1] == 'in-deck'
+		if (!hasClass) card.classList.add('in-deck')
+	}
 }
 
+function showCards () {
+	const cards = document.getElementById('community-cards').children
+	for (let card of cards) {
+		const hasClass = card.classList[1] == 'in-deck'
+		if (hasClass) card.classList.remove('in-deck')
+	}
+}
+
+/**
+ * @param id
+ * Functie om kaart mee om te draaien.
+ * id wordt gebruikt om de juiste kaart te vinden.
+ */
 function toggleCard (id) {
 	const card = document.getElementById(`card-${id}`)
 	card.classList.toggle('in-deck')
